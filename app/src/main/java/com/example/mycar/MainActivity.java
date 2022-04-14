5b1a651144d8de7bb1b36b698a871679800b39a1
Limonchik <nik.monakhov02@mail.ru>
2022-04-14 17:07:10 +0300

    Происходит изменение окон навигации для пользователя(добавлено окна: машина, заправка, расходы, автосервис и напоминания)

--- app/src/main/java/com/example/mycar/MainActivity.java
+++ app/src/main/java/com/example/mycar/MainActivity.java
@@ -24,12 +24,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_car,
+                R.id.navigation_zapravka,
+                R.id.navigation_raskhody,
+                R.id.navigation_autoservise,
                 R.id.navigation_notifications)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
