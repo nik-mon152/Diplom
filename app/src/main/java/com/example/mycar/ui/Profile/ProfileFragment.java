126533a9c0c8d3f6de24ca3e43fc80444374eb65
Limonchik <nik.monakhov02@mail.ru>
2022-04-17 16:33:36 +0300

    Измнен вид вкладки профиль

--- app/src/main/java/com/example/mycar/ui/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/mycar/ui/Profile/ProfileFragment.java
@@ -44,7 +44,6 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
 
 
