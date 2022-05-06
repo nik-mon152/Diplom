68d33e4255438819df39aefdb032938e58abb2eb
Limonchik <nik.monakhov02@mail.ru>
2022-05-06 15:23:16 +0300

    Сделаны незначительные измененияя

--- app/src/main/java/com/example/mycar/ui/Service/AddingService/AddService.java
+++ app/src/main/java/com/example/mycar/ui/Service/AddingService/AddService.java
@@ -1,8 +1,10 @@
 package com.example.mycar.ui.Service.AddingService;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
@@ -17,11 +19,24 @@ public class AddService extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_service);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("Сервис");
 
         autoCompleteTextView = findViewById(R.id.ViewWork);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, viewWork);
 //        autoCompleteTextView.setThreshold(1);
         autoCompleteTextView.setAdapter(adapter);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == androidx.appcompat.R.id.action_bar){
+            return true;
+        }else if (id == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/mycar/ui/Service/InfoServise.java
+++ app/src/main/java/com/example/mycar/ui/Service/InfoServise.java
@@ -1,8 +1,10 @@
 package com.example.mycar.ui.Service;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 import com.example.mycar.R;
@@ -19,10 +21,23 @@ public class InfoServise extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_servise);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("Информация о сервисе");
 
         DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
         String date = df.format(Calendar.getInstance().getTime());
         text = findViewById(R.id.textDate);
         text.setText(date);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == androidx.appcompat.R.id.action_bar){
+            return true;
+        }else if (id == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
