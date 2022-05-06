4cf8cc4d3f7bd1fef3e4c2450d57f0eca6300a14
Limonchik <nik.monakhov02@mail.ru>
2022-05-06 14:24:53 +0300

    Создана разметка для окна с добавлением информации о проходимом сервисе

--- app/src/main/java/com/example/mycar/ui/Service/AddingService/AddService.java
+++ app/src/main/java/com/example/mycar/ui/Service/AddingService/AddService.java
@@ -3,14 +3,25 @@ package com.example.mycar.ui.Service.AddingService;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.example.mycar.R;
 
 public class AddService extends AppCompatActivity {
 
+    AutoCompleteTextView autoCompleteTextView;
+    String[] viewWork = {"ТО", "Ремонт", "Замена", "Тюнинг", "Диагностика", "Шиномонтаж"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_service);
+
+        autoCompleteTextView = findViewById(R.id.ViewWork);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, viewWork);
+//        autoCompleteTextView.setThreshold(1);
+        autoCompleteTextView.setAdapter(adapter);
     }
 }
