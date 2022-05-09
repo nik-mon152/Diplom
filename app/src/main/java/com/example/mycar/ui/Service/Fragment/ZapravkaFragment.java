a983b5276977464694010c6380ffc203c4642a13
Limonchik <nik.monakhov02@mail.ru>
2022-05-09 14:53:04 +0300

    Сделан вид списка заправок

--- app/src/main/java/com/example/mycar/ui/Service/Fragment/ZapravkaFragment.java
+++ app/src/main/java/com/example/mycar/ui/Service/Fragment/ZapravkaFragment.java
@@ -3,24 +3,99 @@ package com.example.mycar.ui.Service.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 
 import com.example.mycar.R;
+import com.example.mycar.ui.Service.ModelZapravka.AdapterZapravka;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ZapravkaFragment extends Fragment {
 
     RecyclerView zapravkaLists;
+    AdapterZapravka adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_zapravka, container, false);
 
         zapravkaLists = v.findViewById(R.id.zapravralist);
+
+        List<String> fuels = new ArrayList<>();
+        List<String> cumms = new ArrayList<>();
+        List<String> litrs = new ArrayList<>();
+        List<String> prices = new ArrayList<>();
+        List<String> probegs = new ArrayList<>();
+        List<String> comments = new ArrayList<>();
+
+        fuels.add("АИ-95");
+        cumms.add("2000Р");
+        litrs.add("50л");
+        prices.add("50Р");
+        probegs.add("120000");
+        comments.add("авпвпвввввпывар");
+
+        fuels.add("АИ-92");
+        cumms.add("1500Р");
+        litrs.add("45л");
+        prices.add("52Р");
+        probegs.add("130000");
+        comments.add("GHbdfg");
+
+        fuels.add("АИ-92");
+        cumms.add("1500Р");
+        litrs.add("45л");
+        prices.add("52Р");
+        probegs.add("130000");
+        comments.add("GHbdfg");
+
+        fuels.add("АИ-92");
+        cumms.add("1500Р");
+        litrs.add("45л");
+        prices.add("52Р");
+        probegs.add("130000");
+        comments.add("GHbdfg");
+
+        fuels.add("АИ-92");
+        cumms.add("1500Р");
+        litrs.add("45л");
+        prices.add("52Р");
+        probegs.add("130000");
+        comments.add("GHbdfg");
+
+        fuels.add("АИ-92");
+        cumms.add("1500Р");
+        litrs.add("45л");
+        prices.add("52Р");
+        probegs.add("130000");
+        comments.add("GHbdfg");
+
+        fuels.add("АИ-92");
+        cumms.add("1500Р");
+        litrs.add("45л");
+        prices.add("52Р");
+        probegs.add("130000");
+        comments.add("GHbdfg");
+
+        fuels.add("АИ-92");
+        cumms.add("1500Р");
+        litrs.add("45л");
+        prices.add("52Р");
+        probegs.add("130000");
+        comments.add("GHbdfg");
+
+        adapter = new AdapterZapravka(fuels, cumms, litrs, prices, probegs, comments);
+        zapravkaLists.setLayoutManager(new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL));
+        zapravkaLists.setAdapter(adapter);
 
         return v;
     }
---
+++ app/src/main/java/com/example/mycar/ui/Service/ModelZapravka/AdapterZapravka.java
@@ -0,0 +1,79 @@
+package com.example.mycar.ui.Service.ModelZapravka;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mycar.R;
+
+import java.util.List;
+
+public class AdapterZapravka extends RecyclerView.Adapter<AdapterZapravka.ViewHolder> {
+    List<String> fuels;
+    List<String> cumms;
+    List<String> litrs;
+    List<String> prices;
+    List<String> probegs;
+    List<String> comments;
+
+    public AdapterZapravka(List<String> fuel, List<String> cumm, List<String> litr,
+                           List<String> price, List<String> probeg, List<String> comment){
+        this.fuels = fuel;
+        this.cumms = cumm;
+        this.litrs = litr;
+        this.prices = price;
+        this.probegs = probeg;
+        this.comments = comment;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.zapravka_view_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.fuelList.setText(fuels.get(position));
+        holder.cummList.setText(cumms.get(position));
+        holder.litrList.setText(litrs.get(position));
+        holder.priceList.setText(prices.get(position));
+        holder.probegList.setText(probegs.get(position));
+        holder.commentList.setText(comments.get(position));
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "click item", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return fuels.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView fuelList, cummList, litrList, priceList, probegList, commentList;
+        View view;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            fuelList = itemView.findViewById(R.id.fuelList);
+            cummList = itemView.findViewById(R.id.cummList);
+            litrList = itemView.findViewById(R.id.litrList);
+            priceList = itemView.findViewById(R.id.priceList);
+            probegList = itemView.findViewById(R.id.prodegList);
+            commentList = itemView.findViewById(R.id.commentList);
+            view = itemView;
+
+        }
+    }
+}
